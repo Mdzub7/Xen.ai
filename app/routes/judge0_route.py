d35e5a2d17e57e01fda80553732647d67635f89f
Rahul.v <svmrahul15@gmail.com>
2025-03-01 13:33:02 +0530

    Removed Auth for Judge0

--- app/routes/judge0_route.py
+++ app/routes/judge0_route.py
@@ -1,15 +1,10 @@
 from fastapi import APIRouter, Depends
 from ..models.code_model import CodeSubmission
 from ..controllers.judge0_controller import process_code_submission
-from ..services.firebase_auth import verify_firebase_token   
 
 router = APIRouter()
 
 @router.post("/run-code/")
-async def run_code(submission: CodeSubmission,
-                   user_data:dict=Depends(verify_firebase_token)):
-    """
-    API endpoint to receive code submissions and return execution results.
-    """
+async def run_code(submission: CodeSubmission):
     result = process_code_submission(submission.source_code, submission.language_id, submission.stdin)
     return result
